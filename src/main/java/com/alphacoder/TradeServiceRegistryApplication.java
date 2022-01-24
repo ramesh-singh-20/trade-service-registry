f17f224e6983711dc57abcb3e3bb76f4792b45c9
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-01-23 19:49:21 -0500

    Initial project set up.

---
+++ src/main/java/com/alphacoder/TradeServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeServiceRegistryApplication.class, args);
+	}
+
+}
